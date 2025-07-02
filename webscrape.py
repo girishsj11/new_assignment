a3cb0d83a8518582b55c6e385dccb38773de3ce7
girishsj11 <girishsj11@gmail.com>
2025-07-02 12:23:22 +0530

    New commit for webscraping concept ,pushing the craped data to csv file.

--- webscrape.py
+++ webscrape.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup as bs
 import requests
+import csv
 
 url = "https://quotes.toscrape.com/"
+file_csv = "authors_quotes.csv"
 page_2_scrape = requests.get(url)
 soup = bs(page_2_scrape.text,"html.parser")
 
 quotes = soup.find_all("span",attrs={"class":"text"})
 authors = soup.find_all("small",attrs={"class":"author"})
 
-for i in range(0,len(quotes)):
-    print(f"Author : {authors[i].text} \nQuotes : {quotes[i].text}\n")
+with open(file_csv,"w") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Sl.No", "Author Name", "Quotes"])
+    for i in range(0,len(authors)):
+        writer.writerow([i+1,authors[i].text,quotes[i].text])
